a606e6e5f38541b2d4eab60a8a1adea153d1b5b7
Lalit <lalitnayyar@gmail.com>
2025-01-16 00:21:30 +0400

    first test

--- introtohuggingfaceinference/index.js
+++ introtohuggingfaceinference/index.js
@@ -3,8 +3,22 @@ import { HfInference } from '@huggingface/inference'
 // Create your Hugging Face Token: https://huggingface.co/settings/tokens
 // Set your Hugging Face Token: https://scrimba.com/dashboard#env
 // Learn more: https://scrimba.com/links/env-variables
-const hf = new HfInference(process.env.HF_TOKEN)
-
+const hf = new HfInference(import.meta.env.VITE_HF_TOKEN)
 // Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
 
 const textToGenerate = "The definition of machine learning inference is "
+
+async function generateText() {
+  const response = await hf.textGeneration({
+    inputs: textToGenerate
+  })
+  console.log(response)
+  const textarea = document.createElement('textarea')
+  textarea.style.backgroundColor = 'yellow'
+  textarea.style.width = '200%'
+  textarea.style.height = '200px'
+  textarea.value = JSON.stringify(response, null, 2)
+  document.body.appendChild(textarea)
+}
+
+generateText()
