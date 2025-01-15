39dcfd9ac3412de6e8b1416c34bc635444c42841
Lalit <lalitnayyar@gmail.com>
2025-01-16 01:33:20 +0400

    transform image with advanced image

--- transformingimageswithhuggingface/index.js
+++ transformingimageswithhuggingface/index.js
@@ -8,15 +8,28 @@ const hf = new HfInference(import.meta.env.VITE_HF_TOKEN)
 // HuggingFace.js Inference docs
 // https://huggingface.co/docs/huggingface.js/inference/README
 
+// HuggingFace.js Inference docs
+// https://huggingface.co/docs/huggingface.js/inference/README
+
 const model = "ghoskno/Color-Canny-Controlnet-model"
 
 const oldImageUrl = "/old-photo.jpeg"
 const oldImageResponse = await fetch(oldImageUrl)
 const oldImageBlob = await oldImageResponse.blob()
 
+const prompt = `An elderly couple walks together on a gravel path with green 
+grass and trees on each side. Wearing neutral-colored clothes, they face away
+ from the camera as they carry their bags.`
+
 const newImageBlob = await hf.imageToImage({
   model: model,
-  inputs: oldImageBlob
+  inputs: oldImageBlob,
+  parameters: {
+    prompt: prompt,
+    negative_prompt: "Black and white photo. text, bad anatomy, blurry, low quality",
+    // Between 0 and 1
+    //strength: 0.6,
+  }
 })
 
 const newImageBase64 = await blobToBase64(newImageBlob)
