91e043cf65991b7f6640c1778102cb5498e8845f
Lalit <lalitnayyar@gmail.com>
2025-01-16 00:34:43 +0400

    textToClassify

--- introtohuggingfaceinference/index.js
+++ introtohuggingfaceinference/index.js
@@ -6,12 +6,12 @@ import { HfInference } from '@huggingface/inference'
 const hf = new HfInference(import.meta.env.VITE_HF_TOKEN)
 // Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
 
-const textToGenerate = "The definition of machine learning inference is "
+const textToClassify = "The definition of machine learning inference is "
 
 async function generateText() {
-  const response = await hf.textGeneration({
-    inputs: textToGenerate,
-    model:"HuggingFaceH4/zephyr-7b-beta"
+    const response = await hf.textClassification({
+    model: "distilbert-base-uncased-finetuned-sst-2-english",
+    inputs: textToClassify
   })
   console.log(response)
   const textarea = document.createElement('textarea')
